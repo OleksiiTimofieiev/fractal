4c71633540c360d504061c18ae7e14fa27febd3b
contrbas@gmail.com <K4rmU78B>
2018-09-11 11:12:49 +0300

    ALL

---
+++ srcs/main.c
@@ -0,0 +1,12 @@
+// TODO: leaks;
+// TODO: norminette;
+// TODO: perror;
+// TODO: types: Julia, Mandelbrot, ?
+// TODO: Julia set without clicking -> change of the parameter;
+// TODO: mouse zoom;
+// TODO: several colors to show the depth of the fractol;
+// TODO: if no parameter is provided or unvalid -> show the list of the parameters and exit;
+// TODO: ESC -> to exit the program;
+// TODO: use images for printing the fina image;
+// TODO: multi threading;
+// TODO: Two valid parameters in the command line, resulting in two fractals in two windows.
