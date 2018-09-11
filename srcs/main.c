0dcf6f2b928c30581664c4e78b9690544e829238
contrbas@gmail.com <K4rmU78B>
2018-09-11 11:33:13 +0300

    ALL

--- srcs/main.c
+++ srcs/main.c
@@ -5,7 +5,6 @@
 // TODO: Julia set without clicking -> change of the parameter;
 // TODO: mouse zoom;
 // TODO: several colors to show the depth of the fractol;
-// TODO: if no parameter is provided or unvalid -> show the list of the parameters and exit;
 // TODO: ESC -> to exit the program;
 // TODO: use images for printing the fina image;
 // TODO: multi threading;
--- srcs/validation.c
+++ srcs/validation.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 11:27:44 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/11 11:29:42 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/11 11:32:52 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,8 @@ static	int		check_fractol_type(char *fractol)
 void			validation(int argc, char *argv)
 {
 	if (argc != 2 || !check_fractol_type(argv))
+	{
 		options();
+		exit(0);
+	}
 }
