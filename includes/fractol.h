da930a28728910e6687554160cdf148987f44dc2
contrbas@gmail.com <K4rmU78B>
2018-09-11 11:41:40 +0300

    ALL

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,12 +6,15 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 11:17:21 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/11 11:33:38 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/11 11:41:17 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
+
+# define MONITOR_WIDTH 5120
+# define MONITOR_HEIGHT 2880
 
 # include "../libft/libft.h"
 # include <mlx.h>
