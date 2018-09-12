b6bdbc4c80ab6d0fd3c59ec8b76ce99396d48e76
contrbas@gmail.com <K4rmU78B>
2018-09-12 16:58:51 +0300

    ALL

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 11:34:17 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/12 16:49:07 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/12 16:58:44 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -171,7 +171,7 @@ void	Mandelbrot(void **mlx_ptr, void **win_ptr, int width, int height)
 					t_hsv _hsv;
 
 					_hsv.H = n % 256;
-					_hsv.S = 120;
+					_hsv.S = 120; //100
 					_hsv.V = 255 * (n < MaxIterations);
 
 					t_rgb rgb = hsv_to_rgb(_hsv);
@@ -182,34 +182,83 @@ void	Mandelbrot(void **mlx_ptr, void **win_ptr, int width, int height)
 			}
 		}
 }
-
-int		main(int argc, char **argv)
+void Julia(void **mlx_ptr, void **win_ptr, int width, int height)
 {
-	int height = 800;
-	int width = 1000;
-	
-	validation(argc, argv[1]);
+	double MinRe = -2.0;
+	double MaxRe = 1.0;
+	double MinIm = -1.2;
+	double MaxIm = MinIm + (MaxRe - MinRe) * height / width;
+	double Re_factor = (MaxRe - MinRe) / (width - 1);
+	double Im_factor = (MaxIm - MinIm) / (height - 1);
 
-	void *mlx_ptr = mlx_init();
-	void *win_ptr = mlx_new_window(mlx_ptr, width, height, "mandelbrot");
-	Mandelbrot(&mlx_ptr, &win_ptr, width, height);
+	int MaxIterations = 30;
 
-	// Mandelbrot2(&mlx_ptr, &win_ptr, width, height);
+	for (int y = 0; y < height; ++y)
+	{
+		double c_im = MaxIm - y * Im_factor;
+		for (int x = 0; x < width; ++x)
+		{
+			double c_re = MinRe + x * Re_factor;
 
-	mlx_loop(mlx_ptr);
+			double Z_re = c_re, Z_im = c_im;
+			int isInside = 0;
+			int n = 0;
+			for (; n < MaxIterations; ++n)
+			{
+				double Z_re2 = Z_re * Z_re;
+				double Z_im2 = Z_im * Z_im;
+				if (Z_re2 + Z_im2 > 4)
+				{
+					isInside = 1;
+					break;
+				}
+				Z_im = 2 * Z_re * Z_im + -0.550;
+				Z_re = Z_re2 - Z_im2 + -0.550;
+			}
+			if (isInside)
+			{
+				t_hsv _hsv;
 
-	// t_hsv data = {154, 0.43, 0.60};
-	// t_rgb value = hsv_to_rgb(data);
+				_hsv.H = n % 256;
+				_hsv.S = 120;
+				_hsv.V = 255 * (n < MaxIterations);
 
-	// printf("R->%d, G->%d, B->%d", value.R, value.G, value.B);
+				t_rgb rgb = hsv_to_rgb(_hsv);
 
-	// t_rgb	rgb;
-	// rgb.R = 245;
-	// rgb.G = 255;
-	// rgb.B = 250;
-
-	// ft_putstr(RGBToHexadecimal(rgb));
-
-	// system("leaks -q fractol");
-	return (0);
+				mlx_pixel_put(*mlx_ptr, *win_ptr, x, y, hex_int_converter(RGBToHexadecimal(rgb)));
+				// putpixel(x, y);
+			}
+		}
+	}
 }
+
+	int main(int argc, char **argv)
+	{
+		int height = 800;
+		int width = 1000;
+
+		validation(argc, argv[1]);
+
+		void *mlx_ptr = mlx_init();
+		void *win_ptr = mlx_new_window(mlx_ptr, width, height, "mandelbrot");
+		// Mandelbrot(&mlx_ptr, &win_ptr, width, height);
+
+		Mandelbrot2(&mlx_ptr, &win_ptr, width, height);
+
+		mlx_loop(mlx_ptr);
+
+		// t_hsv data = {154, 0.43, 0.60};
+		// t_rgb value = hsv_to_rgb(data);
+
+		// printf("R->%d, G->%d, B->%d", value.R, value.G, value.B);
+
+		// t_rgb	rgb;
+		// rgb.R = 245;
+		// rgb.G = 255;
+		// rgb.B = 250;
+
+		// ft_putstr(RGBToHexadecimal(rgb));
+
+		// system("leaks -q fractol");
+		return (0);
+	}
