426939f47bea38327ec5e8b3d35f6c3c7fc3630d
contrbas@gmail.com <K4rmU78B>
2018-09-12 21:36:12 +0300

    ALL

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 11:34:17 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/12 21:20:55 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/12 21:36:03 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,27 +65,8 @@ void	Mandelbrot(void **mlx_ptr, void **win_ptr, int width, int height)
 	// ColorRGB color;							  //the RGB color value for the pixel
 	int maxIterations = 128;				  //after how much iterations the function should stop
 
-	// long double min = -2.84;
-	// long double max = 1.0;
-	// long double factor = 1;
 
-	// zoom = -1;
-
-	clock_t t = 0.0, oldTime, frameTime; //current and old time, and their difference (for input)
 											// int showText = 0;
-
-	//begin main program loop
-	
-	// int count = 0;
-	// while (1)
-	// {
-		// max -= 0.1 * factor;
-		// min += 0.15 * factor;
-		// factor *= 0.9349;
-		// maxIterations += 5;
-		// if (count > 30)
-		// 	maxIterations += 5;
-			// draw the fractal
 		for (int y = 0; y < height; y++) 
 		for (int x = 0; x < width; x++)
 		{
@@ -108,11 +89,6 @@ void	Mandelbrot(void **mlx_ptr, void **win_ptr, int width, int height)
 				if ((newRe * newRe + newIm * newIm) > 4)
 					break;
 			}
-			//use color model conversion to get rainbow palette, make brightness black if maxIterations reached
-			// color = HSVtoRGB(ColorHSV(i % 256, 255, 255 * (i < maxIterations)));
-			//draw the pixel
-			// pset(x, y, color);
-			// generate color func;
 
 			t_hsv _hsv;
 
@@ -125,117 +101,55 @@ void	Mandelbrot(void **mlx_ptr, void **win_ptr, int width, int height)
 			mlx_pixel_put(*mlx_ptr, *win_ptr, x, y, hex_int_converter(RGBToHexadecimal(rgb)));
 
 			}
-		oldTime = clock();
-		t = clock();
-		frameTime = (float)t - oldTime;
 	}
-	// count++;
 
-// }
-
-	void Mandelbrot2(void **mlx_ptr, void **win_ptr, int width, int height)
+	void Julia(void **mlx_ptr, void **win_ptr, int width, int height)
 	{
-		double MinRe = -2.0;
-		double MaxRe = 1.0;
-		double MinIm = -1.2;
-		double MaxIm = MinIm + (MaxRe - MinRe) * height / width;
-		double Re_factor = (MaxRe - MinRe) / (width - 1);
-		double Im_factor = (MaxIm - MinIm) / (height - 1);
-		
-		int MaxIterations = 30;
+		//each iteration, it calculates: new = old*old + c, where c is a constant and old starts at current pixel
 
-		for (int y = 0; y < height; ++y)
-		{
-			double c_im = MaxIm - y * Im_factor;
-			
-			for (int x = 0; x < width; ++x)
+		double newRe, newIm, oldRe, oldIm; //real and imaginary parts of new and old
+		double cRe = -0.7, cIm = 0.27015;
+		// basic zoom;
+		double zoom = 1, moveX = 0.0, moveY = 0; //you can change these to zoom and change position
+		// ColorRGB color;							  //the RGB color value for the pixel
+		int maxIterations = 128; //after how much iterations the function should stop
+
+
+											 // int showText = 0;
+		for (int y = 0; y < height; y++)
+			for (int x = 0; x < width; x++)
 			{
-				double c_re = MinRe + x * Re_factor;
-
-				double Z_re = c_re, Z_im = c_im;
-				int isInside = 0;
-				int n = 0;
-				for (; n < MaxIterations; ++n)
+				//calculate the initial real and imaginary part of z, based on the pixel location and zoom and position values
+				newRe = 1.5 * (x - width / 2) / (0.5 * zoom * width) + (moveX);
+				newIm = (y - height / 2) / (0.5 * zoom * height) + (moveY);
+				// newRe = newIm = oldRe = oldIm = 0; //these should start at 0,0
+				//i will represent the number of iterations
+				int i;
+				//start the iteration process
+				for (i = 0; i < maxIterations; i++)
 				{
-					double Z_re2 = Z_re * Z_re;
-					double Z_im2 = Z_im * Z_im;
-					if (Z_re2 + Z_im2 > 4)
-					{
-						isInside = 1;
+					//remember value of previous iteration
+					oldRe = newRe;
+					oldIm = newIm;
+					//the actual iteration, the real and imaginary part are calculated
+					newRe = oldRe * oldRe - oldIm * oldIm + cRe;
+					newIm = 2 * oldRe * oldIm + cIm;
+					//if the point is outside the circle with radius 2: stop
+					if ((newRe * newRe + newIm * newIm) > 4)
 						break;
-					}
-					Z_im = 2 * Z_re * Z_im + c_im;
-					Z_re = Z_re2 - Z_im2 + c_re;
 				}
-				if (isInside)
-				{
-					t_hsv _hsv;
 
-					_hsv.H = n % 256;
-					_hsv.S = 120;
-					_hsv.V = 255 * (n < MaxIterations);
-
-					t_rgb rgb = hsv_to_rgb(_hsv);
-
-					mlx_pixel_put(*mlx_ptr, *win_ptr, x, y, hex_int_converter(RGBToHexadecimal(rgb)));
-				}
-			}
-		}
-}
-void Julia(void **mlx_ptr, void **win_ptr, int width, int height)
-{
-	double MinRe = -2.0;
-	double MaxRe = 2.0;
-	double MinIm = -2.0;
-	double MaxIm = MinIm + (MaxRe - MinRe) * height / width;
-	
-	double Re_factor = (MaxRe - MinRe) / (width - 1);
-	double Im_factor = (MaxIm - MinIm) / (height - 1);
-
-	int MaxIterations = 400;
-
-	for (int y = 0; y < height; ++y)
-	{
-		double c_im = MaxIm + 0.5 - y * Im_factor;
-		
-		for (int x = 0; x < width; ++x)
-		{
-			double c_re = MinRe + x * Re_factor;
-
-			double Z_re = c_re, Z_im = c_im;
-
-			int isInside = 0;
-			
-			int n = 0;
-			
-			for (; n < MaxIterations; ++n)
-			{
-				double Z_re2 = Z_re * Z_re * 1.2;
-				double Z_im2 = Z_im * Z_im;
-				if (Z_re2 + Z_im2 > 4)
-				{
-					isInside = 1;
-					break;
-				}
-				Z_im = 2 * Z_re * Z_im + 0.400;
-				Z_re = Z_re2 - Z_im2 + 0.400;
-			}
-			if (isInside)
-			{
 				t_hsv _hsv;
 
-				_hsv.H = n % 256;
-				_hsv.S = 120;
-				_hsv.V = 255 * (n < MaxIterations);
+				_hsv.H = i % 256;
+				_hsv.S = 120; //100
+				_hsv.V = 255 * (i < maxIterations);
 
 				t_rgb rgb = hsv_to_rgb(_hsv);
 
 				mlx_pixel_put(*mlx_ptr, *win_ptr, x, y, hex_int_converter(RGBToHexadecimal(rgb)));
-				// putpixel(x, y);
 			}
-		}
 	}
-}
 
 	int main(int argc, char **argv)
 	{
@@ -248,7 +162,7 @@ void Julia(void **mlx_ptr, void **win_ptr, int width, int height)
 		void *win_ptr = mlx_new_window(mlx_ptr, width, height, "mandelbrot");
 		// Mandelbrot(&mlx_ptr, &win_ptr, width, height);
 
-		// Mandelbrot2(&mlx_ptr, &win_ptr, width, height);
+
 		Julia(&mlx_ptr, &win_ptr, width, height);
 
 		mlx_loop(mlx_ptr);
