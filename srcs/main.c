9c0f6205e8b9b849f97e5f407b92c38c4850a014
contrbas@gmail.com <K4rmU78B>
2018-09-12 17:14:13 +0300

    ALL

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 11:34:17 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/12 17:07:54 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/12 17:14:06 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -241,10 +241,10 @@ void Julia(void **mlx_ptr, void **win_ptr, int width, int height)
 
 		void *mlx_ptr = mlx_init();
 		void *win_ptr = mlx_new_window(mlx_ptr, width, height, "mandelbrot");
-		// Mandelbrot(&mlx_ptr, &win_ptr, width, height);
+		Mandelbrot(&mlx_ptr, &win_ptr, width, height);
 
 		// Mandelbrot2(&mlx_ptr, &win_ptr, width, height);
-		Julia(&mlx_ptr, &win_ptr, width, height);
+		// Julia(&mlx_ptr, &win_ptr, width, height);
 
 		mlx_loop(mlx_ptr);
 
