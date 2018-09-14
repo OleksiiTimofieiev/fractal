72409b6ceb8f9c9c1482f23c580ea76b774db51e
contrbas@gmail.com <K4rmU78B>
2018-09-14 12:00:24 +0300

    ALL

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 11:34:17 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/12 21:39:36 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/14 12:00:10 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -152,6 +152,114 @@ void	Mandelbrot(void **mlx_ptr, void **win_ptr, int width, int height)
 			}
 	}
 
+	void Mandelbrot2(void **mlx_ptr, void **win_ptr, int width, int height)
+	{
+		double MinRe = -2.0;
+		double MaxRe = 1.0;
+		double MinIm = -1.2;
+		double MaxIm = MinIm + (MaxRe - MinRe) * height / width;
+		double Re_factor = (MaxRe - MinRe) / (width - 1);
+		double Im_factor = (MaxIm - MinIm) / (height - 1);
+
+		int MaxIterations = 30;
+
+		for (int y = 0; y < height; ++y)
+		{
+			double c_im = MaxIm - y * Im_factor;
+
+			for (int x = 0; x < width; ++x)
+			{
+				double c_re = MinRe + x * Re_factor;
+
+				double Z_re = c_re, Z_im = c_im;
+				int isInside = 0;
+				int n = 0;
+				for (; n < MaxIterations; ++n)
+				{
+					double Z_re2 = Z_re * Z_re;
+					double Z_im2 = Z_im * Z_im;
+					if (Z_re2 + Z_im2 > 4)
+					{
+						isInside = 1;
+						break;
+					}
+					Z_im = 2 * Z_re * Z_im + c_im;
+					Z_re = Z_re2 - Z_im2 + c_re;
+				}
+				if (isInside)
+				{
+					t_hsv _hsv;
+
+					_hsv.H = n % 256;
+					_hsv.S = 120;
+					_hsv.V = 255 * (n < MaxIterations);
+
+					t_rgb rgb = hsv_to_rgb(_hsv);
+
+					mlx_pixel_put(*mlx_ptr, *win_ptr, x, y, hex_int_converter(RGBToHexadecimal(rgb)));
+				}
+			}
+		}
+	}
+	void Julia2(void **mlx_ptr, void **win_ptr, int width, int height)
+	{
+		// borders section;
+		double MinRe = -2.0;
+		double MaxRe = 1.0;
+		double MinIm = -1.2;
+		double MaxIm = MinIm + (MaxRe - MinRe) * height / width;
+
+		// to get the coordinates in the complex numbers;
+		double Re_factor = (MaxRe - MinRe) / (width - 1);
+		double Im_factor = (MaxIm - MinIm) / (height - 1);
+
+		int MaxIterations = 50;
+
+		for (int y = 0; y < height; ++y)
+		{
+			// get the coordinates in the comlex formaat;
+			double c_im = MaxIm - y * Im_factor;
+
+			for (int x = 0; x < width; ++x)
+			{
+				// get the coordinates in the comlex formaat;
+				double c_re = MinRe + x * Re_factor;
+
+				double Z_re = c_re, Z_im = c_im;
+
+				// Final complex numbers according to the coordinates;
+				
+				int isInside = 0;
+				int n = 0;
+				for (; n < MaxIterations; ++n)
+				{
+					double Z_re2 = Z_re * Z_re;
+					double Z_im2 = Z_im * Z_im;
+					
+					if (Z_re2 + Z_im2 > 4)
+					{
+						isInside = 1;
+						break;
+					}
+					Z_im = 2 * Z_re * Z_im + 0.288;
+					Z_re = Z_re2 - Z_im2 + 0.353;
+				}
+				if (isInside)
+				{
+					t_hsv _hsv;
+
+					_hsv.H = n % 256;
+					_hsv.S = 120;
+					_hsv.V = 255 * (n < MaxIterations);
+
+					t_rgb rgb = hsv_to_rgb(_hsv);
+
+					mlx_pixel_put(*mlx_ptr, *win_ptr, x, y, hex_int_converter(RGBToHexadecimal(rgb)));
+				}
+			}
+		}
+	}
+
 	int main(int argc, char **argv)
 	{
 		int height = 800;
@@ -161,10 +269,10 @@ void	Mandelbrot(void **mlx_ptr, void **win_ptr, int width, int height)
 
 		void *mlx_ptr = mlx_init();
 		void *win_ptr = mlx_new_window(mlx_ptr, width, height, "mandelbrot");
-		// Mandelbrot(&mlx_ptr, &win_ptr, width, height);
+		// Mandelbrot2(&mlx_ptr, &win_ptr, width, height);
 
 
-		Julia(&mlx_ptr, &win_ptr, width, height);
+		Julia2(&mlx_ptr, &win_ptr, width, height);
 
 		mlx_loop(mlx_ptr);
 
@@ -184,5 +292,5 @@ void	Mandelbrot(void **mlx_ptr, void **win_ptr, int width, int height)
 		return (0);
 	}
 
-	fr->cRe = sin(6.28 / 720 * (x));
-	fr->cIm = _ABS(cos(6.28 / 720 * (y))) / 3;
+	// fr->cRe = sin(6.28 / 720 * (x));
+	// fr->cIm = _ABS(cos(6.28 / 720 * (y))) / 3;
