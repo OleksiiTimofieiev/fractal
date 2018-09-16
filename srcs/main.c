9f27d0ae52476e36e9274113da10c40158daae95
contrbas@gmail.com <K4rmU78B>
2018-09-16 20:52:32 +0300

    ALL

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 11:34:17 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/16 19:59:35 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/16 20:51:58 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 int main(int argc, char **argv)
 {
 	t_data data;
-	// system("afplay ./sounds/intro.mp3&");
+	// system("afplay ./sounds/Agnus_Dei.mp3&");
 
 	validation(argc, argv[1], &data);
 
@@ -36,7 +36,6 @@ int main(int argc, char **argv)
 	mlx_key_hook(data.m_win_ptr, deal_with_keyboard, &data);
 	mlx_hook(data.m_win_ptr, 6, 1L << 1, mouse_move, &data);
 	mlx_loop(data.m_mlx_ptr);
-	// system("killall afplay");
 
 	return (0);
 }
--- srcs/validation.c
+++ srcs/validation.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 11:27:44 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/16 19:03:05 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/16 20:51:48 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 static	void	options(void)
 {
-	ft_putstr("Options:\n");
+	ft_putstr("Options:\n\n");
 	ft_putstr("1. mandelbrot;\n");
 	ft_putstr("2. julia;\n");
 	ft_putstr("3. cyrcle;\n");
+	ft_putstr("4. mult;\n");
 }
 
 static	int		check_fractol_type(char *fractol)
@@ -28,12 +29,15 @@ static	int		check_fractol_type(char *fractol)
 		return (1);
 	else if (ft_strequ(fractol, "cyrcle"))
 		return (1);
+	else if (ft_strequ(fractol, "mult"))
+		return (1);
 	return (0);
 }
 
 void validation(int argc, char *argv, t_data *data)
 {
-	if (argc != 2 || !check_fractol_type(argv))
+	data->fractol = &mandelbrot;
+	 if (argc != 2 || !check_fractol_type(argv))
 	{
 		options();
 		exit(0);
