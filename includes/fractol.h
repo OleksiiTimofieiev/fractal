daf1dace38edf3e0c832da6748ede037ea73815f
contrbas@gmail.com <K4rmU78B>
2018-09-16 15:45:54 +0300

    ALL

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 11:17:21 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/16 15:45:08 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/16 15:45:37 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ void	validation(int argc, char *argv);
 t_rgb	hsv_to_rgb(t_hsv hsv);
 char	*RGBToHexadecimal(t_rgb rgb);
 void	constructor(t_data *data);
-void mandelbrot(t_data *data);
-void julia(t_data *data);
+void	mandelbrot(t_data *data);
+void	julia(t_data *data);
 
 #endif
