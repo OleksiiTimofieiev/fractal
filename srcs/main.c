71079759eac72dd129ee50148783c054f8008bb2
contrbas@gmail.com <K4rmU78B>
2018-09-16 15:43:51 +0300

    ALL

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 11:34:17 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/16 15:36:11 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/16 15:41:17 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,47 +17,20 @@
 // TODO: ESC -> to exit the program;
 
 // TODO: Plan:
+
 // 4. Image string which is located in the structure;
 // 5. threads;
 // 6. different windows;
 // stop with if;
- // music
- // move without movements;
-
-int hex_int_converter(char *input)
-{
-	int base;
-	size_t len;
-	int dec_val;
-	int i;
-
-	base = 1;
-	len = ft_strlen(input);
-	dec_val = 0;
-	i = len - 1;
-	while (i >= 0)
-	{
-		if (input[i] >= '0' && input[i] <= '9')
-		{
-			dec_val += (input[i] - 48) * base;
-			base = base * 16;
-		}
-		else if (input[i] >= 'A' && input[i] <= 'F')
-		{
-			dec_val += (input[i] - 55) * base;
-			base = base * 16;
-		}
-		i--;
-	}
-	return (dec_val);
-}
+// music
+// move without movements;
 
 
 int zoom(int button, int x, int y, t_data *data)
 {
 	if (x >= 0 && y >= 0 && x <= data->width && y <= data->height && button == 1)
 	{
-		data->zoom += (data->zoom / 10);
+		data->zoom += (data->zoom / 20);
 		data->moveX -= (data->width / 2 - x) / (data->zoom * data->width * 2);
 		data->moveY -= (data->height / 2 - y) / (data->zoom * data->height * 2);
 
@@ -103,8 +76,12 @@ int main(int argc, char **argv)
 	julia(&data);
 	// mandelbrot(&data);
 
+
+	// different func;
+	
 	mlx_hook(data.m_win_ptr, 6, 1L << 1, mouse_move, &data);
 	mlx_hook(data.m_win_ptr, 4, 1L << 1, zoom, &data);
+
 	mlx_loop(data.m_mlx_ptr);
 
 
