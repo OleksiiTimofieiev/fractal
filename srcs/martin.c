cb0e53a83916599b8b87246f31c1fadca8562e23
contrbas@gmail.com <K4rmU78B>
2018-09-17 15:14:24 +0300

    ALL

--- srcs/martin.c
+++ srcs/martin.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/17 13:24:12 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/17 15:00:32 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/17 15:13:57 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,25 +34,28 @@
 // 		}
 // 	}
 
+void fill_pixel_local(char *my_image_string, int x, int y, int num)
+{
+	my_image_string[x * 4 + 4000 * y] = num / (256 ^ 2);
+	my_image_string[x * 4 + 4000 * y + 1] = (num / 256) % 256;
+	my_image_string[x * 4 + 4000 * y + 2] = num % 256;
+}
+
 void martin(t_data *data)
 {
 
 	int x, y, z;
-	int mx, my;
 
 
 
-	mx = data->width / 2;
-	my = data->height / 2;
-
-	for (y = (-my); y <= my; y++)
-		for (x = (-mx); x <= mx; x++)
+	for (y = 0; y < data->height; y++)
+		for (x = 0; x < data->width; x++)
 		{
 			z = (0.1 * data->zoom) * (x * x + y * y);
-			ft_putnbr((int)(z / 16));
-			ft_putstr("\n");
-				mlx_pixel_put(data->m_mlx_ptr, data->m_win_ptr, mx + x, my + y, (int)(z / 16));
+
+			fill_pixel_local(data->mlx_get_data_addr, x, y, (int)(z / 16));
+			// mlx_pixel_put(data->m_mlx_ptr, data->m_win_ptr, mx + x, my + y, (int)(z / 16));
 		}
-			
-		
+
+	mlx_put_image_to_window(data->m_mlx_ptr, data->m_win_ptr, data->mlx_new_image, 0, 0);
 }
