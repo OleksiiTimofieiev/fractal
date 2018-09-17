bbdb04181cba6de3a111b7815658207e4ee77a56
contrbas@gmail.com <K4rmU78B>
2018-09-17 15:17:20 +0300

    ALL

--- srcs/martin.c
+++ srcs/martin.c
@@ -6,56 +6,38 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/17 13:24:12 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/17 15:13:57 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/17 15:16:58 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-
-
-
-// void	martin(t_data *data)
-// {
-// 	double x, t, y;
-// 	int n;
-
-
-// 		x = 0;
-// 		y = 0;
-// 		n = 0;
-// 		while (n < 10 * data->zoom) 
-// 		{
-// 			t = x;
-// 			x = y - sin(x);
-// 			y = 3.14 - t;
-// 			n++;
-// 			mlx_pixel_put(data->m_mlx_ptr, data->m_win_ptr, (x * 2) + 500, floor(y * 2) + 380, 0x008000);
-// 		}
-// 	}
-
-void fill_pixel_local(char *my_image_string, int x, int y, int num)
+void	fill_pixel_local(char *my_image_string, int x, int y, int num)
 {
 	my_image_string[x * 4 + 4000 * y] = num / (256 ^ 2);
 	my_image_string[x * 4 + 4000 * y + 1] = (num / 256) % 256;
 	my_image_string[x * 4 + 4000 * y + 2] = num % 256;
 }
 
-void martin(t_data *data)
+void	martin(t_data *data)
 {
+	int x;
+	int y;
+	int z;
 
-	int x, y, z;
-
-
-
-	for (y = 0; y < data->height; y++)
-		for (x = 0; x < data->width; x++)
+	x = 0;
+	y = 0;
+	z = 0;
+	while (y < data->height)
+	{
+		x = 0;
+		while (x < data->width)
 		{
-			z = (0.1 * data->zoom) * (x * x + y * y);
-
+			z = (0.01 * data->zoom) * (x * x + y * y);
 			fill_pixel_local(data->mlx_get_data_addr, x, y, (int)(z / 16));
-			// mlx_pixel_put(data->m_mlx_ptr, data->m_win_ptr, mx + x, my + y, (int)(z / 16));
+			x++;
 		}
-
+		y++;
+	}
 	mlx_put_image_to_window(data->m_mlx_ptr, data->m_win_ptr, data->mlx_new_image, 0, 0);
 }
