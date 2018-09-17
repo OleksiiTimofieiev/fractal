7ef8c779e0c5eefb440ea4115c4f777dc1d43e80
contrbas@gmail.com <K4rmU78B>
2018-09-17 14:35:14 +0300

    ALL

--- srcs/martin.c
+++ srcs/martin.c
@@ -1,44 +1,59 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cyrcle.c                                           :+:      :+:    :+:   */
+/*   martin.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/17 13:24:12 by otimofie          #+#    #+#             */
-/*   Updated: 2018/09/17 14:25:09 by otimofie         ###   ########.fr       */
+/*   Updated: 2018/09/17 14:35:08 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
 
-			void fill(char *my_image_string, int x, int y)
-			{
-				my_image_string[x * 4 + 4000 * y] = 0;
-				my_image_string[x * 4 + 4000 * y + 1] = 80;
-				my_image_string[x * 4 + 4000 * y + 2] = 0;
-			}
 
-void	martin(t_data *data)
+
+// void	martin(t_data *data)
+// {
+// 	double x, t, y;
+// 	int n;
+
+
+// 		x = 0;
+// 		y = 0;
+// 		n = 0;
+// 		while (n < 10 * data->zoom) 
+// 		{
+// 			t = x;
+// 			x = y - sin(x);
+// 			y = 3.14 - t;
+// 			n++;
+// 			mlx_pixel_put(data->m_mlx_ptr, data->m_win_ptr, (x * 2) + 500, floor(y * 2) + 380, 0x008000);
+// 		}
+// 	}
+
+#define a 1
+#define b 3
+#define dt 5
+
+void martin(t_data *data)
 {
-	double x, t, y;
-	int n;
+	int i, j, k;
+	float x, y;
 
-
-		x = 0;
-		y = 0;
-		n = 0;
-		while (n < 10 * data->zoom) 
+	for (i = 0; i <= 43; i++)
+		for (j = 0; j <= 37; j++)
 		{
-			t = x;
-			x = y - sin(x);
-			y = 3.14 - t;
-			n++;
-			mlx_pixel_put(data->m_mlx_ptr, data->m_win_ptr, (x * 2) + 500, floor(y * 2) + 380, 0x008000);
+			x = i;
+			y = j;
+			for (k = 1; k <= 100; k++)
+			{
+				y = y + sin(x + a * sin(b * x)) * dt;
+				x = x - sin(y + a * sin(b * y)) * dt;
+				// putpixel(int(x*15), int(y*15), (i+j)%10);
+				mlx_pixel_put(data->m_mlx_ptr, data->m_win_ptr, (int)(x * 23), (int)(y * 21), (0x008000));
+			}
 		}
-	}
-
-
-		
-
+}
